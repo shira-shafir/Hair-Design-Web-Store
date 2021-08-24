cf6a737c0f1f1478e5cd79e37e14c520dda25920
Shira <shafirshira@gmail.com>
2021-08-24 20:49:34 +0300

    new project

---
+++ src/server.js
@@ -0,0 +1,96 @@
+/* jshint esversion: 6 */
+"use strict";
+const express = require("express");
+// const redis = require('redis');
+const {v4: uuidv4} = require('uuid');
+import * as module from "module";
+const userFile = require ("./usersDB.json");
+const productFile = require("./productDB.json");
+
+//cart
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const json = require('json');
+const {logDOM} = require("@testing-library/react");
+const {err_code} = require("redis/lib/utils");
+
+// redis.createClient({host: "127.0.0.1", port: 6379});
+
+const port = process.env.PORT || 3005;
+const app = express();
+// const client = redis.createClient({host: "127.0.0.1", port: 6379});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+// app."cors"
+// app.use.cookies
+
+app.use(express.json());
+
+app.get("/users",((req, res) => {
+    res.status(200).send(userFile);
+}))
+app.get("/products",((req, res) => {
+    res.status(200).send(productFile);
+}))
+
+app.post(`/register`, async (req, res, next) => {
+    
+    if(req.body.username.valueOf()===undefined||req.body.password.valueOf()===undefined ){
+        res.err("undefined");
+    }
+    
+    let id = genarateUuid();
+    // createUser
+    //add to db
+    let user = {id:id,username:req.body.username,password:req.body.password,cart:[],purchases:[],login:[],sessions:[],isAdmin: false};
+    userFile.append(user);
+    res.status(200).json(user);
+    }
+)
+
+
+app.post(`/login`,(req, res, next) => {
+    if ("user is connected"){
+        return " "
+    }
+    if ("user doesn't exist"){
+        return ""
+    }
+    if ("there is required data undefined"){
+        return ""
+    }
+    //check user info matches the one in db
+    if ("password incorrect"){
+        return "incorrect password"
+    }
+    // get to it -login data ix סעיף
+})
+
+const getAllTypeAsObj = (type, callback) => {
+    // get all type from redis
+    return redisClient.hgetall(type, (err, results) => {
+        // results is built as user_key + JSON(object)
+        if (err) console.log(err);
+        else callback(getallType(results));// tranform into array of objects
+    });
+};
+
+const getallType = (items) => {
+    if (items === undefined || items === null) return items;
+    return Object.values(items).map(i => JSON.parse(i));// object of object (key+value) -> array of objects values(JSON) -> array of objects values(OBJ)
+};
+
+app.listen(port, () => {
+    console.log(`http://localhost:${port}`);
+})
+
+
+// cookies and sessions
+
+
+//register
+const genarateUuid = () => {
+    return uuidv4();
+}
+
