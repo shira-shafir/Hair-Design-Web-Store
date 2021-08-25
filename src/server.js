3ef5dd59cf91cdbc4fac0469191106276eb20138
taldanai1995 <70094025+taldanai1995@users.noreply.github.com>
2021-08-25 16:29:08 +0300

    update

--- src/server.js
+++ src/server.js
@@ -43,7 +43,7 @@ app.post(`/register`, async (req, res, next) => {
     const filePath = path.join(process.cwd(), 'usersDB.json');
     const fileData = fs.readFileSync(filePath);
     const data = JSON.parse(fileData);
-    if (user.username === getUsers().username || user.username === undefined ||user.password === undefined ){
+    if (user.username.valueOf() === getUsers().username  ||user.password.valueOf() === undefined ){
         console.log("error, user exist");
         res.status(500).json("ERR");
         return;
