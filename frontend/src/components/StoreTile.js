cef110ed717774dbac329410f752c570ba8d4c8a
Shira <shafirshira@gmail.com>
2021-09-02 13:57:51 +0300

    designed StoreTile.js and Quiz

--- frontend/src/components/StoreTile.js
+++ frontend/src/components/StoreTile.js
@@ -1,21 +1,61 @@
 import {useState} from "react";
 import styled from 'styled-components';
+import likeButton from '../assets/likeButton.jpg';
 
 const TileContainer = styled.div`
-  background-color: grey;
-  height: 200px;
-  width: 200px;
+  background-color: #00aabb;
+  height: 350px;
+  width: 350px;
   overflow: hidden;
-  
+  outline: none;
+
+
+  & h1 {
+    font-family: inherit;
+    font-size: 24px;
+    font-weight: 500;
+    text-align: center;
+    line-height: 1.2;
+  }
+
+  & h5 {
+    text-align: center;
+  }
+
+  & p {
+    text-align: center;
+  }
+
   & img {
+    display: flex;
     width: 100%;
-    height: 100%;
+    height: 50%;
     object-fit: cover;
     overflow: hidden;
+    justify-content: center;
+    text-align: center;
   }
-  
+
   & button {
-    color: green;
+    margin: 0;
+    position: relative;
+    top: 50%;
+    -ms-transform: translateY(-100%);
+    transform: translateX(200%);
+    //transform: translateY(-50%);
+    display: flex;
+    text-align: center;
+    align-items: center;
+    justify-content: center;
+    border: hidden;
+    outline: 0;
+    padding: 12px;
+    color: white;
+    background-color: #000;;
+    cursor: pointer;
+    width: 12%;
+    font-size: 20px;
+    box-sizing: content-box;
   }
 `;
 
@@ -31,7 +71,7 @@ export function StoreTile({name, description, image, price,addToCartCallback,lik
         <h1>{name}</h1>
         <p>{description}</p>
         <h5>Price: {price}$</h5>
-        {likeCallback && <button onClick={likeCallback}>Like</button>}
+        {likeCallback && <button onClick={likeCallback}><img src={likeButton}/></button>}
         {addToCartCallback &&  <button onClick={addToCartCallback}>Add to cart</button>}
       </div>}
     </TileContainer>
